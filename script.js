fc7c8edc3f91bd9107869456cc6b9d2428b35592
ZBatess <bate0011@student.acitech.org>
2025-03-21 09:13:10 -0400

    HELP MEF#LIUYFLKUTDCLKUTCLUTCLUTCIIOUTDCOUTD

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+const playGame = () => {
+const rock = document.getElelemenyById("rock");
+const scissors = document.getElelemenyById("scissors");
+const paper = document.getElelemenyById("paper");
+const coworker = document.getElelemenyById("annoyingcoworker");
+const laptop = document.getElelemenyById("laptop");
+const playerOptions = [rock, paper, scissors, coworker, laptop];
+const computerOptions = ['rock', 'paper', 'scissors', coworker, laptop]
+}
+
 function getcomputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
+    const choices = ['rock', 'paper', 'scissors', 'annoyingcoworker', 'laptop'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }
 
-  fuction () findWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-        return "it's a tie!!!"
-    }
-  }
+  playerOptions.forEach(option => {
+    option.addEventListener('click', function () {
+      const choiceNumber = Math.floor(Math.random() * 5);
+const ComputerChoice = computerOptions[choiceNumber];
+}
