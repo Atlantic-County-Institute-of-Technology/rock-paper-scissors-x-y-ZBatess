545b9c43c7f0d29cc964371d93400a85e4cdf4f0
ZBatess <bate0011@student.acitech.org>
2025-03-25 09:10:27 -0400

    im losing my mind

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
+const rock = document.getElementById("rock");
+const scissors = document.getElementById("scissors");
+const paper = document.getElementById("paper");
+const coworker = document.getElementById("annoyingcoworker");
+const laptop = document.getElementById("laptop");
+const playerOptions = ['rock', 'paper', 'scissors', 'coworker', 'laptop'];
+const computerOptions = ['rock', 'paper', 'scissors', 'coworker', 'laptop']
 
-const playGame = () => {
-const rock = document.getElelemenyById("rock");
-const scissors = document.getElelemenyById("scissors");
-const paper = document.getElelemenyById("paper");
-const coworker = document.getElelemenyById("annoyingcoworker");
-const laptop = document.getElelemenyById("laptop");
-const playerOptions = [rock, paper, scissors, coworker, laptop];
-const computerOptions = ['rock', 'paper', 'scissors', coworker, laptop]
+console.log(coworker)
+
+let playerScore = 0;
+let computerScore = 0;
+let playChoice = 0;
+let cpuChoice = 0;
+
+  
+  
+rock.addEventListener("click", () => {
+  playerChoice(1);
+})
+paper.addEventListener("click", () => {
+  playerChoice(2);
+})
+scissors.addEventListener("click", () => {
+  playerChoice(3);
+})
+coworker.addEventListener("click", () => {
+  playerChoice(4);
+})
+laptop.addEventListener("click", () => {
+  playerChoice(5);
+})
+
+scissors.addEventListener("click", playerScissors)
+paper.addEventListener("click", playerPaper)
+coworker.addEventListener("click", Playerannoyingcoworker)
+laptop.addEventListener("click", playerLaptop)
+  
+function playerChoice(choice) {
+  playChoice = choice;
 }
 
-const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-  let moves = 0;
+// TODO: Make a CPU Choice function that has the CPU Choose a random number from 1-5
+function cpuChoose() {
+  Math.floor(Math.random() * 5) + 1;
+}
 
-  const playGame = () => {
-      const rockBtn = document.querySelector('.rock');
-      const paperBtn = document.querySelector('.paper');
-      const scissorBtn = document.querySelector('.scissor');
-      const annoyingcoworkerBtn = document.querySelector('.annoyingcoworker');
-      const laptopBtn = document.querySelector('.laptop');
-      const playerOptions = [rockBtn, paperBtn, scissorBtn, annoyingcoworkerBtn, laptopBtn ];
-      const computerOptions = ['rock', 'paper', 'scissors', 'annoyingcoworker', 'laptop']
-
-playerOptions.forEach(option => {
-  option.addEventListener('click', function () {
-
-  const winner = (player, computer) => {
-    const result = document.querySelector('.result');
-    const playerScoreBoard = document.querySelector('.p-count');
-    const computerScoreBoard = document.querySelector('.c-count');
-    player = player.toLowerCase();
-    computer = computer.toLowerCase();
-    if (player === computer) {
-        result.textContent = 'Tie'
-    }
-    else if (player == 'rock') {
-      if (computer == 'paper') {
-        result.textContent = 'cpmputr won';
-        
+// TODO: Make a function for "playing the game" that takes the playChoice and cpuChoice and compares them
+function playGame() {
+}
