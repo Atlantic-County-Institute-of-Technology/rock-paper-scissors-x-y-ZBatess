f0a12608d599ec8102290ebbb2dbb51d178a0da3
ZBatess <bate0011@student.acitech.org>
2025-03-26 09:13:16 -0400

    final commit.
    it doesn't work, i don't know what to do to actually make the game function.
    
    im sorry

--- script.js
+++ script.js
@@ -5,31 +5,20 @@ const coworker = document.getElementById("annoyingcoworker");
 const laptop = document.getElementById("laptop");
 const playerOptions = ['rock', 'paper', 'scissors', 'coworker', 'laptop'];
 const computerOptions = ['rock', 'paper', 'scissors', 'coworker', 'laptop']
-
-console.log(coworker)
+let player = document.getElementById("player")
+let playerpick = 0
+let player_score = document.getElementById("playerScore")
+let playerwins = 0
+let computer = document.getElementById("computer")
+let result = document.getElementById("result")
+let cpu_score = document.getElementById("computerScore")
+let cpuwins = 0
 
 let playerScore = 0;
 let computerScore = 0;
 let playChoice = 0;
 let cpuChoice = 0;
 
-  
-  
-rock.addEventListener("click", () => {
-  playerChoice(1);
-})
-paper.addEventListener("click", () => {
-  playerChoice(2);
-})
-scissors.addEventListener("click", () => {
-  playerChoice(3);
-})
-coworker.addEventListener("click", () => {
-  playerChoice(4);
-})
-laptop.addEventListener("click", () => {
-  playerChoice(5);
-})
 
 scissors.addEventListener("click", playerScissors)
 paper.addEventListener("click", playerPaper)
@@ -41,10 +30,19 @@ function playerChoice(choice) {
 }
 
 // TODO: Make a CPU Choice function that has the CPU Choose a random number from 1-5
-function cpuChoose() {
-  Math.floor(Math.random() * 5) + 1;
+function computerChoice() {
+  Math.floor(Math.random() * 4.99) + 1;
 }
 
 // TODO: Make a function for "playing the game" that takes the playChoice and cpuChoice and compares them
-function playGame() {
+function pickpaper() {
+  player.innerHTML = '<src="buttons paper" alt="✋" class="display">';
+   playerpick = 1
+  cpuchoice = computerchoice()
+}
+function papercheck1(){
+  if (cpuchoice == 1 ) {
+      computer.innerHTML = '<src="buttons paper" alt="✋" class="cpu_display">';
+      result.innerHTML = "its a tie^^";
+  }
 }
